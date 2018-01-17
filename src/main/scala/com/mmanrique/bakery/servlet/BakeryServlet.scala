ac8a760f613f71e78d828d59846f63fab1213b7d
Manrique <moporto@amazon.com>
2018-01-16 16:24:50 -0800

    Revert "Using scalate as template engine"
    This reverts commit 4a4fe0a

--- src/main/scala/com/mmanrique/bakery/servlet/BakeryServlet.scala
+++ src/main/scala/com/mmanrique/bakery/servlet/BakeryServlet.scala
@@ -1,12 +1,9 @@
 package com.mmanrique.bakery.servlet
 
 import org.scalatra.ScalatraServlet
-import org.scalatra.scalate.ScalateSupport
 
-class BakeryServlet extends ScalatraServlet with ScalateSupport {
-
-  get("/") {
-    contentType = "text/html"
-    ssp("/index")
+class BakeryServlet extends ScalatraServlet {
+  get("/"){
+    <h1>Hello Ponin</h1>
   }
 }
